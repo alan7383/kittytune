b912c904627dcbb3da509cb032682fa6e2660c72
alan7383 <alan.mussot@outlook.fr>
2026-05-07 17:13:26 +0200

    chore: migrate to github actions and remove docs folder

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://alan7383.github.io',
   base: '/kittytune/',
-  output: 'static',
-  outDir: '../docs',
   build: {
     format: 'file',
   },
