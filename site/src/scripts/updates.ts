26595e85bb2a4bc3487c81a14fc5f77ace4307dc
alan7383 <alan.mussot@outlook.fr>
2026-05-18 18:21:48 +0200

    feat(site): align M3 expressive loader with exact Android Kotlin canvas source specifications

--- site/src/scripts/updates.ts
+++ site/src/scripts/updates.ts
@@ -204,9 +204,9 @@ function showSkeleton() {
     <div class="timeline-loading-state">
       <h3 class="m3-loader-text">Checking for updates...</h3>
       <div class="m3-expressive-loader">
-        <div class="m3-loader-clip">
-          <div class="m3-loader-track"></div>
-          <div class="m3-loader-bar"><div class="m3-loader-wave"></div></div>
+        <div class="m3-loader-track"></div>
+        <div class="m3-loader-bar">
+          <div class="m3-loader-wave"></div>
         </div>
       </div>
     </div>
