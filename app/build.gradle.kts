bfb81e8f884d700cb0936be23e5f366722eedc67
alan7383 <alan.mussot@outlook.fr>
2026-05-23 12:44:49 +0200

    chore: bump version to 2.42.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.alananasss.kittytune"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
-        versionName = "2.41.0"
+        versionCode = 22
+        versionName = "2.42.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
