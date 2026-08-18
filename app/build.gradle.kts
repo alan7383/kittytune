6eff6fef8f35d9ec9e51ce6256d6242eeac39fe9
alan7383 <alan.mussot@outlook.fr>
2026-08-18 15:48:19 +0200

    chore(release): bump version to 2.60.0 (46)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.alananasss.kittytune"
         minSdk = 26
         targetSdk = 37
-        versionCode = 45
-        versionName = "2.59.0"
+        versionCode = 46
+        versionName = "2.60.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
